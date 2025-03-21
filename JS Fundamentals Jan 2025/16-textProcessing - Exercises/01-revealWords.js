dbbb197d6699a0761effb48627a40bf00f155844
IvanIvanovJS <sibsmartco@gmail.com>
2025-03-21 09:46:19 +0200

    Create 01-revealWords.js

---
+++ JS Fundamentals Jan 2025/16-textProcessing - Exercises/01-revealWords.js
@@ -0,0 +1,24 @@
+function revealWords(words, sentence) {
+    let wordsArr = words
+        .split(`, `)
+        .sort((a, b) => b.length - a.length);
+
+
+    console.log(wordsArr);
+
+    for (let word of wordsArr) {
+        let wordLength = word.length;
+        let pattern = "*".repeat(wordLength);
+        if (sentence.includes(pattern)) {
+            sentence = sentence.replace(pattern, word);
+        }
+
+
+    }
+    console.log(sentence);
+
+}
+revealWords('new, great, learning',
+    'softuni is ***** place for ******** new programming languages'
+
+)
